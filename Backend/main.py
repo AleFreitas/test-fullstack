454cea7264d7807fa95533316b8dc7d4bea068f1
Alexandre de Freitas <alexandrefsoficial@gmail.com>
2024-11-12 22:23:24 -0300

    feat/backend: cors, envs, sqlalchemy and other base features added to main file

--- Backend/main.py
+++ Backend/main.py
@@ -1,9 +1,34 @@
+# System Imports
+import os
+
+# Third-party Imports
 from flask import Flask
-from views.api import api_blueprint
-import views.users  # Importing the views/users.py file to guarantee the routes are registered
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+# Module Imports
+
+from models import db
+from controllers.api import api_blueprint
+import controllers.users  # to guarantee that the routes are registered
+import utils.errors # to guarantee that the error handlers are registered
+
+load_dotenv()
 
 def create_app():
     flask_app = Flask(__name__)
+
+    CORS(flask_app, origins=["*"])
+
+
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://user:password@localhost/mydatabase')
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(flask_app)
+
+    with flask_app.app_context():
+        db.create_all()
 
     flask_app.register_blueprint(api_blueprint)
 
@@ -16,4 +41,4 @@ def create_app():
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
