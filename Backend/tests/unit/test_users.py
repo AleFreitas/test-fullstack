c643e91f376569e1911b90f49bd8880a52ff8d36
Alexandre de Freitas <alexandrefsoficial@gmail.com>
2024-11-13 14:45:37 -0300

    tests/Backend: tests for insert service implemented

--- Backend/tests/unit/test_users.py
+++ Backend/tests/unit/test_users.py
@@ -5,10 +5,15 @@ from unittest.mock import MagicMock, patch
 import pytest
 from werkzeug.exceptions import BadRequest, Conflict
 
-from app.services.user_services import find_all_users, update_user
+from app.services.user_services import find_all_users, update_user, insert_user
 from tests.mocks.user_mocks import mock_user_list, mock_user_data
 
 from conftest import mock_session
+
+
+# ---------------------------------------------------------------------------- #
+# --------------------------- Service: find_users ---------------------------- #
+# ---------------------------------------------------------------------------- #
 
 # WHEN find_all_users SHOULD return all users
 @patch("app.services.user_services.get_all_users", return_value=mock_user_list)
@@ -19,6 +24,11 @@ def test_find_all_users(mock_get_all_users, app):
     expected_result = mock_user_list
     assert result == expected_result
     mock_get_all_users.assert_called_once()
+
+
+# ---------------------------------------------------------------------------- #
+# --------------------------- Service: update_user --------------------------- #
+# ---------------------------------------------------------------------------- #
 
 # WHEN update_user AND user exists AND new email is not used SHOULD return user data
 @patch('app.services.user_services.get_user_by_id', return_value=mock_user_data)
@@ -56,3 +66,27 @@ def test_update_user_new_email_already_registered(mock_get_user_by_id, mock_get_
     mock_get_user_by_id.assert_called_once()
     mock_get_user_by_email.assert_called_once()
 
+# ---------------------------------------------------------------------------- #
+# --------------------------- Service: insert_user --------------------------- #
+# ---------------------------------------------------------------------------- #
+
+# WHEN insert_user AND user exists AND new email is not used SHOULD return user data
+@patch('app.services.user_services.get_user_by_email', return_value=None)
+@patch('app.services.user_services.create_new_user', return_value=mock_user_data)
+def test_insert_user_success(mock_create_new_user, mock_get_user_by_email, app):
+    with app.app_context():
+        result = insert_user(mock_user_data)
+    
+    assert result == mock_user_data
+
+    mock_get_user_by_email.assert_called_once()
+    mock_create_new_user.assert_called_once()
+
+# WHEN insert_user BUT new email is already registered SHOULD raise Conflict
+@patch('app.services.user_services.get_user_by_email', return_value=mock_user_data)
+def test_insert_user_new_email_already_registered(mock_get_user_by_email, app):
+    with app.app_context():
+        with pytest.raises(Conflict, match="User already exists"):
+            insert_user(mock_user_data)
+    
+    mock_get_user_by_email.assert_called_once()
