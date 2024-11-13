3d17152ae60f93766e4f23d0c590462c5cca5ec7
Alexandre de Freitas <alexandrefsoficial@gmail.com>
2024-11-13 17:58:41 -0300

    feat: tailwind implemented

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import AppRoutes from "./routes/app";
 import "./globalStyles.css"; // Adicione esta linha
+import "./index.css"
 
 const App: React.FC = () => {
   return (
--- frontend/src/components/molecules/Header.tsx
+++ frontend/src/components/molecules/Header.tsx
@@ -5,20 +5,11 @@ import React from 'react';
 const Header: React.FC = () => {
     return (
         <Box
-            sx={{
-                width: '100vw',
-                height: '90px',
-                backgroundColor: '#333333',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center'
-            }}
+            className="w-screen h-[90px] bg-[#333333] flex justify-center items-center"    
         >
             <Box
                 component={'img'}
-                sx={{
-                    height: '65px'
-                }}
+                className="h-[65px]"
                 src={"/uolHeader.png"}
             />
         </Box>
